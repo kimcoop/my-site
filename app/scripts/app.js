ea75b100402992fc13ed03f133a2ddfaba3dceba
Kim Cooperrider <kim.cooperrider@gmail.com>
2014-01-25 17:46:56 -0500

    get those animations twerkin

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,4 +29,26 @@ myApp
       element.css('top', -(scrolled * 0.2) + 'px');
     });
   };
+})
+
+.directive("liScroll", function ($window) {
+  return function(scope, element, attrs) {
+    var windowEl = angular.element($window);
+    windowEl.bind("scroll", function() {
+
+      var body = document.querySelector('body'),
+        bodyEl = angular.element(body);
+
+      var outerHeight = windowEl[0].outerHeight,
+        elInView = ( element[0].offsetTop - windowEl[0].scrollY ) < ( outerHeight / 2),
+        pageScrollComplete = (( windowEl[0].innerHeight + windowEl[0].scrollY) >= bodyEl[0].offsetHeight );
+
+      if ( elInView || pageScrollComplete ) {
+        element.addClass('in');
+      } else {
+        element.removeClass('in');
+      }
+
+    });
+  };
 });
